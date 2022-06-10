eadaee32a07f0a3859af48ca96d587d071c30e58
Peter Runge <prunge@causal.net.au>
2022-06-10 23:55:02 +1000

    Just some exploratory testing on the disco API.

--- src/test/java/au/net/causal/maven/plugins/autojdk/TestDisco.java
+++ src/test/java/au/net/causal/maven/plugins/autojdk/TestDisco.java
@@ -11,11 +11,13 @@ import eu.hansolo.jdktools.versioning.VersionNumber;
 import io.foojay.api.discoclient.DiscoClient;
 import io.foojay.api.discoclient.pkg.Distribution;
 import io.foojay.api.discoclient.pkg.Pkg;
+import io.foojay.api.discoclient.util.Helper;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 class TestDisco
 {
@@ -24,19 +26,6 @@ class TestDisco
     throws Exception
     {
         DiscoClient discoClient = new DiscoClient();
-
-        //Wait for client to be initialized
-        while (!discoClient.isInitialzed())
-        {
-            Thread.sleep(500L);
-        }
-        System.out.println("Now initialized");
-
-        Map<String, Distribution> distros = discoClient.getDistros();
-        System.out.println(distros);
-
-        if (true)
-            return;
 
         /*
         List<Distribution> distributions = discoClient.getDistributions();
@@ -52,8 +41,8 @@ class TestDisco
         System.out.println("---------------------------------------");
 
         //List<Pkg> pkgResult = discoClient.getPkgs(null, new VersionNumber(17), null, OperatingSystem.WINDOWS, null, Architecture.AMD64, Bitness.BIT_64, null, PackageType.JDK, null, true, null, null, null, null, null);
-        //List<Pkg> pkgResult = discoClient.getPkgs(null, new VersionNumber(17), Latest.AVAILABLE, OperatingSystem.WINDOWS, null, Architecture.AMD64, Bitness.BIT_64, null, PackageType.JDK, null, true, null, null, null, null, null);
-        List<Pkg> pkgResult = discoClient.getPkgs(null, new VersionNumber(17), Latest.ALL_OF_VERSION, OperatingSystem.WINDOWS, null, Architecture.AMD64, Bitness.BIT_64, null, PackageType.JDK, null, true, null, null, null, null, null);
+        List<Pkg> pkgResult = discoClient.getPkgs(null, new VersionNumber(17), Latest.AVAILABLE, OperatingSystem.WINDOWS, null, Architecture.AMD64, Bitness.BIT_64, null, PackageType.JDK, null, true, null, null, null, null, null);
+        //List<Pkg> pkgResult = discoClient.getPkgs(null, new VersionNumber(17), Latest.ALL_OF_VERSION, OperatingSystem.WINDOWS, null, Architecture.AMD64, Bitness.BIT_64, null, PackageType.JDK, null, true, null, null, null, null, null);
         System.out.println(pkgResult);
 
 
@@ -65,5 +54,42 @@ class TestDisco
 
 
         }
+    }
+
+    @Test
+    void testInitialization() throws InterruptedException
+    {
+        DiscoClient discoClient = new DiscoClient();
+
+        //Wait for client to be initialized
+        while (!discoClient.isInitialzed())
+        {
+            Thread.sleep(500L);
+        }
+        System.out.println("Now initialized");
+
+        Map<String, Distribution> distros = discoClient.getDistros();
+        System.out.println(distros);
+    }
+
+    @Test
+    void testPreloadWithoutNetwork() throws ExecutionException, InterruptedException
+    {
+        //Could use these (local only) for groupId/artifactId generation for known JDKs
+        Map<String, Distribution> dists = Helper.preloadDistributions().get();
+        for (Map.Entry<String, Distribution> entry : dists.entrySet())
+        {
+            System.out.println(entry.getKey() + ":");
+            System.out.println(entry.getValue());
+        }
+    }
+
+    @Test
+    void currentOs()
+    {
+        OperatingSystem os = eu.hansolo.jdktools.util.Helper.getOperatingSystem();
+        System.out.println("Current OS: " + os);
+        Architecture arch = eu.hansolo.jdktools.util.Helper.getArchitecture();
+        System.out.println("Arch: " + arch);
     }
 }
