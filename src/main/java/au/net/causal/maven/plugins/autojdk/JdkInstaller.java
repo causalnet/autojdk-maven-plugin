23161c74887708b8fb950c187979261bdf062d5f
Peter Runge <prunge@causal.net.au>
2022-06-23 19:09:32 +1000

    JDK installer extraction work.

--- src/main/java/au/net/causal/maven/plugins/autojdk/JdkInstaller.java
+++ src/main/java/au/net/causal/maven/plugins/autojdk/JdkInstaller.java
@@ -1,41 +1,72 @@
 package au.net.causal.maven.plugins.autojdk;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.util.ArchiveEntryUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class JdkInstaller
 {
+    private static final Logger log = LoggerFactory.getLogger(JdkInstaller.class);
+
     private final ArchiveStreamFactory archiveStreamFactory = ArchiveStreamFactory.DEFAULT;
+    private final CompressorStreamFactory compressorStreamFactory = CompressorStreamFactory.getSingleton();
 
     /**
      * All these tools must be found under the JDK directory, optionally with a '.exe' suffix for Windows.
      * Directory separators normalized to '/'.
      */
-    private final Collection<String> knownJdkTools = List.of("bin/java", "bin/javac");
+    private static final Collection<String> knownJdkTools = List.of("bin/java", "bin/javac");
 
-    public void installJdkArchive(Path jdkArchive)
+    private final Path jdksInstallationDirectory;
+
+    public JdkInstaller(Path jdksInstallationDirectory)
+    {
+        this.jdksInstallationDirectory = Objects.requireNonNull(jdksInstallationDirectory);
+    }
+
+    public Path installJdkArchive(Path jdkArchive, String name)
     throws IOException
     {
         String topLevelDir = detectTopLevelDirectory(jdkArchive);
+        if (topLevelDir == null)
+        {
+            log.warn("Could not detect top-level JDK directory in " + jdkArchive);
+            topLevelDir = "";
+        }
 
-        System.out.println(topLevelDir);
+        Path jdkExtractionDir = jdksInstallationDirectory.resolve(name);
+        if (Files.exists(jdkExtractionDir))
+            throw new FileAlreadyExistsException(jdkExtractionDir.toString());
 
-        //TODO extractArchive(jdkArchive, topLevelDir, outputDir);
+        extractArchive(jdkArchive, topLevelDir, jdkExtractionDir);
+
+        return jdkExtractionDir;
     }
 
     /**
@@ -47,7 +78,8 @@ public class JdkInstaller
      *
      * @throws IOException if an error occurs scanning the archive.
      */
-    private String detectTopLevelDirectory(Path jdkArchive)
+    @VisibleForTesting
+    String detectTopLevelDirectory(Path jdkArchive)
     throws IOException
     {
         //Find the shortest path to bin/java[.exe] and bin/javac[.exe]
@@ -55,8 +87,11 @@ public class JdkInstaller
         Set<String> filesInArchive = new LinkedHashSet<>();
         Set<String> directoriesInArchive = new LinkedHashSet<>(); //All will end with '/'
 
+        //Always allow the root
+        directoriesInArchive.add("");
+
         //Read all directories in archive
-        try (ArchiveInputStream is = archiveStreamFactory.createArchiveInputStream(new BufferedInputStream(Files.newInputStream(jdkArchive))))
+        try (ArchiveInputStream is = detectAndReadArchiveFile(jdkArchive))
         {
             ArchiveEntry entry;
             while ((entry = is.getNextEntry()) != null)
@@ -105,5 +140,163 @@ public class JdkInstaller
         }
 
         return bestDirectory;
+    }
+
+    private void extractArchive(Path jdkArchive, String baseDirectoryInArchive, Path outputDirectory)
+    throws IOException
+    {
+        try (ArchiveInputStream is = detectAndReadArchiveFile(jdkArchive))
+        {
+            ArchiveEntry entry;
+            while ((entry = is.getNextEntry()) != null)
+            {
+                extractFile(entry, is, baseDirectoryInArchive, outputDirectory);
+            }
+        }
+        catch (ArchiveException e)
+        {
+            throw new IOException("Error reading archive: " + jdkArchive + ": " + e.getMessage(), e);
+        }
+    }
+
+    private void extractFile(ArchiveEntry entry, InputStream entryData, String baseDirectoryInArchive, Path baseOutputDirectory)
+    throws IOException
+    {
+        //Do not extract this entry if it doesn't start with the base directory inside the archive
+        if (!entry.getName().startsWith(baseDirectoryInArchive))
+            return;
+
+        //Remove leading '/' if it exists and normalize separators to system
+        String relativeEntryName = entry.getName().substring(baseDirectoryInArchive.length());
+        relativeEntryName = FilenameUtils.separatorsToUnix(relativeEntryName);
+        if (relativeEntryName.startsWith("/"))
+            relativeEntryName = relativeEntryName.substring(1);
+
+        relativeEntryName = FilenameUtils.separatorsToSystem(relativeEntryName);
+
+        //Generate a target path
+        Path targetPath = baseOutputDirectory.resolve(relativeEntryName);
+
+        //Extract file to this path
+        extractEntryToTargetPath(entry, entryData, targetPath);
+    }
+
+    private void extractEntryToTargetPath(ArchiveEntry entry, InputStream entryData, Path targetPath)
+    throws IOException
+    {
+        if (entry.isDirectory())
+            Files.createDirectories(targetPath);
+        else
+        {
+            //Might not have explicit directory entries, so make sure parent exists before extracting
+            Files.createDirectories(targetPath.getParent());
+
+            //Copy the data
+            try (OutputStream targetOs = Files.newOutputStream(targetPath))
+            {
+                entryData.transferTo(targetOs);
+            }
+
+            //File modified date
+            if (entry.getLastModifiedDate() != null)
+                Files.setLastModifiedTime(targetPath, FileTime.from(entry.getLastModifiedDate().toInstant()));
+
+            //Unix permissions
+            if (!entry.isDirectory())
+            {
+                Integer unixMode = readUnixModeForArchiveEntry(entry);
+                if (unixMode != null)
+                {
+                    try
+                    {
+                        ArchiveEntryUtils.chmod(targetPath.toFile(), unixMode); //Same thing what Maven/Plexus archiver does
+                    }
+                    catch (ArchiverException e)
+                    {
+                        //Prefer checked exception
+                        throw new IOException(e);
+                    }
+
+                }
+            }
+        }
+    }
+
+    protected Integer readUnixModeForArchiveEntry(ArchiveEntry entry)
+    {
+        if (entry instanceof TarArchiveEntry)
+        {
+            TarArchiveEntry tarEntry = (TarArchiveEntry)entry;
+            if (tarEntry.getMode() != 0)
+                return tarEntry.getMode();
+            else
+                return null;
+        }
+        else if (entry instanceof ZipArchiveEntry)
+        {
+            ZipArchiveEntry zipEntry = (ZipArchiveEntry)entry;
+            if (zipEntry.getUnixMode() != 0)
+                return zipEntry.getUnixMode();
+            else
+                return null;
+        }
+
+        //Couldn't read mode if we get here
+        return null;
+    }
+
+    /**
+     * Opens an archive file, detects its format, and opens an archive input stream to read its contents.
+     *
+     * @param archiveFile the archive file to open.
+     *
+     * @return the opened archive stream ready to read its entries.
+     *
+     * @throws IOException if an I/O error occurs reading the file.
+     * @throws ArchiveException if an error occurs understanding the archive format.
+     */
+    private ArchiveInputStream detectAndReadArchiveFile(Path archiveFile)
+    throws IOException, ArchiveException
+    {
+        String compressor;
+
+        InputStream is = new BufferedInputStream(Files.newInputStream(archiveFile));
+        try
+        {
+
+            //Check if there is a compressor (the .gz in a tar.gz) and decompress if we can
+            //Might not need a compressor
+            compressor = CompressorStreamFactory.detect(is);
+        }
+        catch (CompressorException e)
+        {
+            //No compressor found for the stream, just treat as an archive directly
+            compressor = null;
+        }
+        catch (Exception e)
+        {
+            //Another worse exception, bail out
+            try
+            {
+                is.close();
+            }
+            catch (IOException ex)
+            {
+                e.addSuppressed(ex);
+            }
+            throw e;
+        }
+
+        try
+        {
+            if (compressor != null)
+                is = new BufferedInputStream(compressorStreamFactory.createCompressorInputStream(compressor, is)); //Need another buffered because mark() is required for archive stream
+        }
+        catch (CompressorException e)
+        {
+            throw new IOException("Could not decompress file " + archiveFile + ": " + e.getMessage(), e);
+        }
+
+        return archiveStreamFactory.createArchiveInputStream(is);
     }
 }
---
+++ src/test/java/au/net/causal/maven/plugins/autojdk/TestJdkInstaller.java
@@ -0,0 +1,213 @@
+package au.net.causal.maven.plugins.autojdk;
+
+import com.google.common.base.StandardSystemProperty;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.glassfish.jaxb.core.v2.TODO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TestJdkInstaller
+{
+    private JdkInstaller installer;
+
+    @TempDir
+    private Path tempDir;
+
+    private Path jdksInstallationDirectory;
+
+    @BeforeEach
+    private void setUpJdkInstaller()
+    throws IOException
+    {
+        jdksInstallationDirectory = Files.createTempDirectory(tempDir, "autojdk-test-jdks");
+        installer = new JdkInstaller(jdksInstallationDirectory);
+    }
+
+    @Test
+    void test()
+    throws IOException
+    {
+        //TODO need a better way of picking a filename - this is only temporary
+        Path archiveFile = Path.of(StandardSystemProperty.USER_HOME.value(), ".m2", "repository", "au", "net", "causal", "autojdk", "jdk", "zulu", "17.0.3-7", "zulu-17.0.3-7-windows-x64.zip");
+        Path jdkDir = installer.installJdkArchive(archiveFile, "myjdk");
+
+        System.out.println("Extracted JDK to: " + jdkDir);
+
+        //TODO tests to verify output
+    }
+
+    private Path generateZipArchiveWithEmptyEntries(String... entryPaths)
+    throws IOException, ArchiveException
+    {
+        Path archiveFile = Files.createTempFile(tempDir, "autojdktest", ".zip");
+        try (ArchiveOutputStream os = ArchiveStreamFactory.DEFAULT.createArchiveOutputStream(ArchiveStreamFactory.ZIP, Files.newOutputStream(archiveFile)))
+        {
+            for (String entryPath : entryPaths)
+            {
+                ArchiveEntry entry = new ZipArchiveEntry(entryPath);
+                os.putArchiveEntry(entry);
+                os.closeArchiveEntry();
+            }
+            os.finish();
+        }
+
+        return archiveFile;
+    }
+
+    private Path generateTarGzArchiveWithEmptyEntries(String... entryPaths)
+    throws IOException, ArchiveException, CompressorException
+    {
+        Path archiveFile = Files.createTempFile(tempDir, "autojdktest", ".tar.gz");
+        try (ArchiveOutputStream os = ArchiveStreamFactory.DEFAULT.createArchiveOutputStream(ArchiveStreamFactory.TAR,
+                                         CompressorStreamFactory.getSingleton().createCompressorOutputStream(CompressorStreamFactory.GZIP,
+                                            Files.newOutputStream(archiveFile))))
+        {
+            for (String entryPath : entryPaths)
+            {
+                ArchiveEntry entry = new TarArchiveEntry(entryPath);
+                os.putArchiveEntry(entry);
+                os.closeArchiveEntry();
+            }
+            os.finish();
+        }
+
+        return archiveFile;
+    }
+
+    @Nested
+    class DetectTopLevelDirectory
+    {
+        @Test
+        void everythingInTopLevelZipWindows()
+        throws IOException, ArchiveException
+        {
+            Path archiveFile = generateZipArchiveWithEmptyEntries(
+                    "readme.txt", "jre/", "jre/something.txt", //other stuff
+                    "bin/", "bin/java.exe", "bin/javaw.exe", "bin/javac.exe" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("");
+        }
+
+        @Test
+        void everythingInTopLevelZipLinux()
+        throws IOException, ArchiveException
+        {
+            Path archiveFile = generateZipArchiveWithEmptyEntries(
+                    "readme.txt", "jre/", "jre/something.txt", //other stuff
+                    "bin/", "bin/java", "bin/javaw", "bin/javac" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("");
+        }
+
+        @Test
+        void everythingInTopLevelTarGzWindows()
+        throws IOException, ArchiveException, CompressorException
+        {
+            Path archiveFile = generateTarGzArchiveWithEmptyEntries(
+                    "readme.txt", "jre/", "jre/something.txt", //other stuff
+                    "bin/", "bin/java.exe", "bin/javaw.exe", "bin/javac.exe" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("");
+        }
+
+        @Test
+        void everythingInTopLevelTarGzLinux()
+        throws IOException, ArchiveException, CompressorException
+        {
+            Path archiveFile = generateTarGzArchiveWithEmptyEntries(
+                    "readme.txt", "jre/", "jre/something.txt", //other stuff
+                    "bin/", "bin/java", "bin/javaw", "bin/javac" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("");
+        }
+
+        @Test
+        void oneDirectoryDownZipWindows()
+        throws IOException, ArchiveException
+        {
+            Path archiveFile = generateZipArchiveWithEmptyEntries(
+                    "hello.txt", "doc/", "doc/readme.html", //other stuff at top level
+                    "myjdk/", "myjdk/readme.txt", "myjdk/jre/", "myjdk/jre/something.txt", //other stuff
+                    "myjdk/bin/", "myjdk/bin/java.exe", "myjdk/bin/javaw.exe", "myjdk/bin/javac.exe" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("myjdk/");
+        }
+
+        @Test
+        void oneDirectoryDownZipLinux()
+        throws IOException, ArchiveException
+        {
+            Path archiveFile = generateZipArchiveWithEmptyEntries(
+                    "hello.txt", "doc/", "doc/readme.html", //other stuff at top level
+                    "myjdk/", "myjdk/readme.txt", "myjdk/jre/", "myjdk/jre/something.txt", //other stuff
+                    "myjdk/bin/", "myjdk/bin/java", "myjdk/bin/javaw", "myjdk/bin/javac" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("myjdk/");
+        }
+
+        @Test
+        void oneDirectoryDownTarGzWindows()
+        throws IOException, ArchiveException, CompressorException
+        {
+            Path archiveFile = generateTarGzArchiveWithEmptyEntries(
+                    "hello.txt", "doc/", "doc/readme.html", //other stuff at top level
+                    "myjdk/", "myjdk/readme.txt", "myjdk/jre/", "myjdk/jre/something.txt", //other stuff
+                    "myjdk/bin/", "myjdk/bin/java.exe", "myjdk/bin/javaw.exe", "myjdk/bin/javac.exe" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("myjdk/");
+        }
+
+        @Test
+        void oneDirectoryDownTarGzLinux()
+        throws IOException, ArchiveException, CompressorException
+        {
+            Path archiveFile = generateTarGzArchiveWithEmptyEntries(
+                    "hello.txt", "doc/", "doc/readme.html", //other stuff at top level
+                    "myjdk/", "myjdk/readme.txt", "myjdk/jre/", "myjdk/jre/something.txt", //other stuff
+                    "myjdk/bin/", "myjdk/bin/java", "myjdk/bin/javaw", "myjdk/bin/javac" //the tools
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("myjdk/");
+        }
+
+        @Test
+        void multipleCandidatesZipWindows()
+        throws IOException, ArchiveException
+        {
+            Path archiveFile = generateZipArchiveWithEmptyEntries(
+                    "hello.txt", "doc/", "doc/readme.html", //other stuff at top level
+
+                    //jre - should not be chosen because it is not the shortest path to all the tools
+                    //(normally wouldn't have javac but whatever - we are testing finding shortest path)
+                    "myjdk/", "myjdk/jre/", "myjdk/jre/bin/", "myjdk/jre/bin/java.exe", "myjdk/jre/bin/javac.exe",
+
+                    //The shorter paths that should be chosen
+                    "myjdk/bin/", "myjdk/bin/java.exe", "myjdk/bin/javaw.exe", "myjdk/bin/javac.exe"
+            );
+            String topLevel = installer.detectTopLevelDirectory(archiveFile);
+            assertThat(topLevel).isEqualTo("myjdk/");
+        }
+    }
+}
