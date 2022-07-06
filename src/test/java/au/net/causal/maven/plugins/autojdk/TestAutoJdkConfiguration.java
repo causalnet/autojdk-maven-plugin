e054ef8b43309dac4f8fc46cf7006c68165d58e7
Peter Runge <prunge@causal.net.au>
2022-07-06 19:09:33 +1000

    More tests for AutoJdkConfiguration JAXB serialization and deserialization.

--- src/test/java/au/net/causal/maven/plugins/autojdk/TestAutoJdkConfiguration.java
+++ src/test/java/au/net/causal/maven/plugins/autojdk/TestAutoJdkConfiguration.java
@@ -2,21 +2,49 @@ package au.net.causal.maven.plugins.autojdk;
 
 import jakarta.xml.bind.JAXB;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
+
+import static org.assertj.core.api.Assertions.*;
 
 class TestAutoJdkConfiguration
 {
+    private static final Logger log = LoggerFactory.getLogger(TestAutoJdkConfiguration.class);
+
     @Test
     void testSerialization()
+    throws IOException
     {
         AutoJdkConfiguration config = new AutoJdkConfiguration();
         config.getVendors().add("zulu");
         config.getVendors().add("*");
 
-        StringWriter out = new StringWriter();
-        JAXB.marshal(config, out);
+        try (StringWriter out = new StringWriter())
+        {
+            JAXB.marshal(config, out);
+            log.debug(out.toString());
+            assertThat(out.toString()).contains("<vendor>zulu</vendor>", "<vendor>*</vendor>");
+        }
+    }
 
-        System.out.println(out);
+    @Test
+    void testDeserialization()
+    {
+        String xml = "<autojdk-configuration><vendors>" +
+                     "    <vendor>zulu</vendor>" +
+                     "    <vendor>*</vendor>" +
+                     "</vendors></autojdk-configuration>";
+
+        try (StringReader in = new StringReader(xml))
+        {
+            AutoJdkConfiguration result = JAXB.unmarshal(in, AutoJdkConfiguration.class);
+            log.debug(result.getVendors().toString());
+
+            assertThat(result.getVendors()).containsExactly("zulu", "*");
+        }
     }
 }
