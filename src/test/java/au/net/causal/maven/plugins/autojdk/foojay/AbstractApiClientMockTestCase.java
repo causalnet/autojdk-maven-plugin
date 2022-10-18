a9396d03c020477ff886ea4d7c8ace9c1a11cb17
Peter Runge <prunge@causal.net.au>
2022-10-18 19:01:08 +1100

    Allow pass-through for wiremock test case to allow testing with real service without recording.

--- src/test/java/au/net/causal/maven/plugins/autojdk/foojay/AbstractApiClientMockTestCase.java
+++ src/test/java/au/net/causal/maven/plugins/autojdk/foojay/AbstractApiClientMockTestCase.java
@@ -16,19 +16,22 @@ import static com.github.tomakehurst.wiremock.stubbing.StubImport.*;
  * <p>
  *
  * Test Foojay client but use Wiremock playback to avoid smashing their server in our tests.
- * When run with {@link #WIREMOCK_RECORD} as false (the default) it will use a previously recorded session and avoid
- * hitting their servers at all.  Turning it to true will instead hit the real server and also record responses that can be played back later.
+ * When run with {@link #wireMockMode} as {@link WireMockMode#PLAYBACK} (the default) it will use a previously recorded session and avoid
+ * hitting their servers at all.  Turning it to {@link WireMockMode#RECORD} will instead hit the real server and also record responses that can be played back later.
+ * {@link WireMockMode#REAL_SERVICE} will just hit the real service without recording, useful for ensuring the real service hasn't changed since the last recording.
  * Recording may be required when changing test code since different services may be hit.
  */
 @WireMockTest
 class AbstractApiClientMockTestCase
 {
     /**
-     * Set this to true to record from the real system so we can play back later.
+     * Controls whether
      */
-    static final boolean WIREMOCK_RECORD = false;
+    static final WireMockMode wireMockMode = WireMockMode.PLAYBACK;
 
     protected static ApiClient apiClient = new FoojayClient().getApiClient(); //Make a FoojayClient just so we can get the default configured API client
+
+    private static final String defaultFoojayApiUrl = FoojayClient.createDefaultApiClient().getBaseUri();
 
     @BeforeAll
     static void setUpDisco(WireMockRuntimeInfo wireMockRuntimeInfo)
@@ -36,30 +39,49 @@ class AbstractApiClientMockTestCase
         //Proxy mode with HTTPS is hard to set up so just do explicit URLs for now
         //apiClient.setHttpClientBuilder(HttpClient.newBuilder().proxy(ProxySelector.of(new InetSocketAddress("localhost", wireMockRuntimeInfo.getHttpPort()))));
 
-        apiClient.updateBaseUri(wireMockRuntimeInfo.getHttpBaseUrl());
+        if (wireMockMode != WireMockMode.REAL_SERVICE)
+            apiClient.updateBaseUri(wireMockRuntimeInfo.getHttpBaseUrl());
     }
 
     @BeforeEach
     void setUpWireMock(WireMockRuntimeInfo wireMockRuntimeInfo)
     {
-        if (WIREMOCK_RECORD)
+        if (wireMockMode == WireMockMode.RECORD)
         {
             wireMockRuntimeInfo.getWireMock().importStubMappings(stubImport().ignoreExisting());
             wireMockRuntimeInfo.getWireMock().startStubRecording(
-                    new RecordSpecBuilder().forTarget("https://api.foojay.io")
+                    new RecordSpecBuilder().forTarget(defaultFoojayApiUrl)
                                            .ignoreRepeatRequests()
                                            .onlyRequestsMatching(anyRequestedFor(urlMatching(".*/disco/.*"))));
         }
 
         //Record from api.foojay.io which is the real server
-        if (WIREMOCK_RECORD)
-            stubFor(get(urlMatching("/.*")).willReturn(aResponse().proxiedFrom("https://api.foojay.io")));
+        if (wireMockMode == WireMockMode.RECORD)
+            stubFor(get(urlMatching("/.*")).willReturn(aResponse().proxiedFrom(defaultFoojayApiUrl)));
     }
 
     @AfterEach
     void finishWireMock(WireMockRuntimeInfo wireMockRuntimeInfo)
     {
-        if (WIREMOCK_RECORD)
+        if (wireMockMode == WireMockMode.RECORD)
             wireMockRuntimeInfo.getWireMock().stopStubRecording();
+    }
+
+    protected static enum WireMockMode
+    {
+        /**
+         * Play back previously recorded responses.
+         */
+        PLAYBACK,
+
+        /**
+         * Perform requests to real service and record requests and responses.
+         */
+        RECORD,
+
+        /**
+         * Perform requests to real service without recording responses.
+         */
+        REAL_SERVICE
     }
 }
