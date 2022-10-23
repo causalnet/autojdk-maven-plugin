c5270166765131ac02cd8ca37c7beb5c0a540228
Peter Runge <prunge@causal.net.au>
2022-10-23 12:04:49 +1100

    Fix running purge cache test on non Linux-x64 OSes, allow arch/os to be specified for purge JDKs goal.

--- src/main/java/au/net/causal/maven/plugins/autojdk/PurgeJdksMojo.java
+++ src/main/java/au/net/causal/maven/plugins/autojdk/PurgeJdksMojo.java
@@ -1,5 +1,7 @@
 package au.net.causal.maven.plugins.autojdk;
 
+import eu.hansolo.jdktools.Architecture;
+import eu.hansolo.jdktools.OperatingSystem;
 import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -28,6 +30,20 @@ public class PurgeJdksMojo extends AbstractAutoJdkMojo
     private String purgeJdkVendor;
 
     /**
+     * The system architecture of the JDK to remove.  Use a value of 'all' to match all architectures.  Defaults to the current system architecture.
+     * e.g. 'x64', 'aarch64'
+     */
+    @Parameter(property = "autojdk.purge.architecture")
+    private String purgeJdkArchitecture;
+
+    /**
+     * The operating system of the JDK to remove.  Use a value of 'all' to match all operating systems.  Defaults to the current operating system.
+     * e.g. 'windows', 'linux'
+     */
+    @Parameter(property = "autojdk.purge.os")
+    private String purgeJdkOperatingSystem;
+
+    /**
      * Whether to match normal releases (GA) or early access releases (EA) when purging.
      */
     @Parameter(property = "autojdk.purge.releaseType", defaultValue = "GA", required = true)
@@ -39,7 +55,10 @@ public class PurgeJdksMojo extends AbstractAutoJdkMojo
     @Parameter(property = "autojdk.purge.caches", defaultValue = "true", required = true)
     private boolean purgeCaches;
 
-    private static final String PURGE_ALL_VENDOR = "all";
+    /**
+     * Wildcard value that specifies to select all vendors, architectures or operating systems.
+     */
+    private static final String PURGE_ALL_VALUE = "all";
 
     @Override
     public void execute()
@@ -48,10 +67,26 @@ public class PurgeJdksMojo extends AbstractAutoJdkMojo
         super.execute();
 
         String vendorPurgeFilter;
-        if (PURGE_ALL_VENDOR.equals(purgeJdkVendor))
+        if (PURGE_ALL_VALUE.equals(purgeJdkVendor))
             vendorPurgeFilter = null;
         else
             vendorPurgeFilter = purgeJdkVendor;
+
+        Architecture architecturePurgeFilter;
+        if (purgeJdkArchitecture == null)
+            architecturePurgeFilter = platformTools.getCurrentArchitecture();
+        else if (PURGE_ALL_VALUE.equals(purgeJdkArchitecture))
+            architecturePurgeFilter = null;
+        else
+            architecturePurgeFilter = Architecture.fromText(purgeJdkArchitecture);
+
+        OperatingSystem osPurgeFilter;
+        if (purgeJdkOperatingSystem == null)
+            osPurgeFilter = platformTools.getCurrentOperatingSystem();
+        else if (PURGE_ALL_VALUE.equals(purgeJdkOperatingSystem))
+            osPurgeFilter = null;
+        else
+            osPurgeFilter = OperatingSystem.fromText(purgeJdkOperatingSystem);
 
         VersionRange purgeJdkVersionRange;
         try
@@ -64,8 +99,8 @@ public class PurgeJdksMojo extends AbstractAutoJdkMojo
         }
 
         JdkSearchRequest jdkSearchRequest =  new JdkSearchRequest(purgeJdkVersionRange,
-                                                                  platformTools.getCurrentArchitecture(),
-                                                                  platformTools.getCurrentOperatingSystem(),
+                                                                  architecturePurgeFilter,
+                                                                  osPurgeFilter,
                                                                   vendorPurgeFilter,
                                                                   purgeJdkReleaseType);
         try
